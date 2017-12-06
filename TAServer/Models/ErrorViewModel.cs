659d34842a7ee95d7524fecbce9d87b4a59e33c2
Matus Vanco <mvanco@intesys.cz>
2017-12-06 15:32:18 +0100

    Add project files.

---
+++ TAServer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TAServer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
